0aa81547200946b9b171f960cddf3f3f52e1fed6
Thoma77s <thomassiffc3@gmail.com>
2021-12-14 16:47:27 -0500

    reset value when generating password

--- friendly-parakeet-main/friendly-parakeet-main/Develop/script.js
+++ friendly-parakeet-main/friendly-parakeet-main/Develop/script.js
@@ -1,32 +1,37 @@
 // Assignment code here
-var passwordArray = []
 var passwordLength = "8 < i < 128";
-var password = ""
-var input;
-var confirmNumber;
-var confirmCharacter;
-var confirmUppercase;
-var confrimLowercase;
+
 
 uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 number = ["1","2","3","4","5","6","7","8","9","0"];
 character = ["!","@","#","$","%","^","&","*","?","<",">","+","=","_","-",]
-choices = []
+
 
 function generatePassword (){
+  var choices = [];
+  var passwordArray = [];
+  var password = "";
+  var input;
+  var confirmNumber;
+  var confirmCharacter;
+  var confirmUppercase;
+  var confirmLowercase;
   input=(prompt("How many long do you want your password to be? Choose between 8 and 128."));
   if (!input) {
     alert("This needs to be a value!")
-    return false;
+    return generatePassword();
   } else if (input < 8 || input > 128) {
     alert("You must choose between 8 and 128!");
   } else {
     confirmNumber = confirm("Will this contain numbers?");
-    confirmCharacter = confirm("Whill this contain special characters?");
+    confirmCharacter = confirm("Will this contain special characters?");
     confirmUppercase = confirm("Will this contain uppercase letters?");
     confirmLowercase = confirm("Will this contain lowercase letters?")
   };
+  
+
+
 
   if (!confirmNumber && !confirmCharacter && !confirmUppercase && !confirmLowercase) {
     alert("You need to select criteria!");
@@ -47,7 +52,7 @@ function generatePassword (){
 
   var passwordLength = input
   
-  for(i=0; i <= passwordLength; i++) {
+  for(i=0; i < passwordLength; i++) {
     var pickChoices = choices[Math.floor(Math.random() * choices.length)];
     passwordArray.push(pickChoices)  
   }
@@ -55,6 +60,7 @@ function generatePassword (){
   password = passwordArray.join("") 
 
   return password;
+
 
 }
 
@@ -72,4 +78,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword,);
